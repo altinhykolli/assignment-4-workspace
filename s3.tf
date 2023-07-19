c2ff83ee5e3bdfb766a5afe58188a92d02825c24
altinhykolli <altinhykolli@gmail.comm>
2023-07-19 19:23:32 +0200

    starting again, s3 module configured

--- s3.tf
+++ s3.tf
@@ -1,33 +1,5 @@
-resource "aws_s3_bucket" "altin-assignment" {
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "altin-assignment-4"
-
-  tags = {
-    Name = "Altin Assignment 4"
+  acl    = "private"
   }
-
-  website {
-    index_document = "index.html"
-  }
-}
-
-resource "aws_cloudfront_origin_access_identity" "my_identity" {
-  comment = "CloudFront Origin Access Identity for altin-assignment-4"
-}
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.altin-assignment.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.my_identity.iam_arn
-        },
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.altin-assignment.arn}/*",
-      },
-    ],
-  })
-}

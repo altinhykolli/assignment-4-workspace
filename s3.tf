d49df0f640a4f10fc03a5ccb2064f0366f1ae203
altinhykolli <altinhykolli@gmail.comm>
2023-07-19 19:32:10 +0200

    configuring s3,removed acl

--- s3.tf
+++ s3.tf
@@ -1,14 +1,4 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "altin-assignment-4"
-    grant = [{
-    type       = "CanonicalUser"
-    permission = "FULL_CONTROL"
-    id         = data.aws_canonical_user_id.current.id
-    }, {
-    type       = "CanonicalUser"
-    permission = "FULL_CONTROL"
-    id         = data.aws_cloudfront_log_delivery_canonical_user_id.cloudfront.id # Ref. https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html
-    }
-  ]
   }

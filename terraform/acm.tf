582e66a521420ed92960407072fa0d54a20fda9b
altinhykolli <altinhykolli@gmail.comm>
2023-07-21 20:05:58 +0200

    commented acm for a fix

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,11 +1,8 @@
-module "acm" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "~> 4.0"
-
-  domain_name  = local.domain_name
-  zone_id      = local.hosted_zone_id
-  subject_alternative_names = local.alternative_domain_names
-
-  wait_for_validation = true
-
-}
+#module "acm" {
+#  source  = "terraform-aws-modules/acm/aws"
+#  version = "~> 4.0"
+#  domain_name  = local.domain_name
+#  zone_id      = local.hosted_zone_id
+#  subject_alternative_names = local.alternative_domain_names
+#  wait_for_validation = true
+#}

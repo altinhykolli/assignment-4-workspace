ad8e08bc72e965f569ff7b1c0f8ed308f1b000ef
altinhykolli <altinhykolli@gmail.comm>
2023-07-21 20:15:02 +0200

    commented acm

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,8 +1,11 @@
-module "acm" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "~> 4.0"
-  domain_name  = local.domain_name
-  zone_id      = local.hosted_zone_id
-  subject_alternative_names = local.alternative_domain_names
-  wait_for_validation = true
-}
+#module "acm" {
+#  source  = "terraform-aws-modules/acm/aws"
+#  version = "4.3.2"
+#  providers = {
+#    aws = aws.us_east_1
+#  }
+#  domain_name  = local.domain_name
+#  zone_id      = local.hosted_zone_id
+#  subject_alternative_names = local.alternative_domain_names
+#  wait_for_validation = true
+#}
